2ac66c76289c7d1703dfe1d3654246c8a18ca58a
atikroman2019 <atikroman2016@gmail.comgmail.com>
2025-09-26 20:58:50 +0600

    Primary Setup Done

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-3xl font-bold text-blue-400'>
+            Hello world!
+        </div>
+    );
+};
+
+export default Home;
